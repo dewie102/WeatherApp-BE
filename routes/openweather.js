28a64363ceede6bd5d50657888bb83626c0ea78a
Zack Romasz <zack.romasz@tlgcohort.com>
2023-10-17 17:24:05 -0400

    Format data sent to the frontend

--- routes/openweather.js
+++ routes/openweather.js
@@ -8,6 +8,8 @@ const openWeatherAPIKey = process.env.OPENWEATHER_API;
 router.get("/", async (req, res) => {
     const { lat, lon } = req.query;
 
+    console.log("Hit openweather backend API, lon/lat");
+
     if (!lat || !lon) {
         return res.status(400).json({
             error: "You need to provide both lat and lon of location",
@@ -20,6 +22,8 @@ router.get("/", async (req, res) => {
 
 router.get("/zip", async (req, res) => {
     let { zipcode, countryCode } = req.query;
+
+    console.log("Hit openweather backend API, zip");
 
     if (!zipcode) {
         return res.status(400).json({
@@ -66,7 +70,7 @@ function formatCurrentWeatherResponseForFrontend(weatherData) {
         temp: weatherData.main.temp,
         humidity: weatherData.main.humidity,
         windSpeed: weatherData.wind.speed,
-        description: weatherData.weather.main,
+        description: weatherData.weather[0].main,
         iconUrl: `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`,
     };
 
