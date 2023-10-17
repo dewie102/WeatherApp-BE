cefb3e62d331be46f156f0615645c7207c6ef83d
Zack Romasz <zackariah.romasz@yahoo.com>
2023-10-17 16:06:31 -0400

    Got calls to the backend using zip and long/lat working on current weather

--- routes/openweather.js
+++ routes/openweather.js
@@ -1,9 +1,67 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    console.log("Routing on /api/weatherapp/openweather/");
-    res.send("Routing on /api/weatherapp/openweather/");
+const openWeatherBaseUrl = "https://api.openweathermap.org/data/2.5/weather";
+const geocodeBaseURL = "http://api.openweathermap.org/geo/1.0";
+const openWeatherAPIKey = process.env.OPENWEATHER_API;
+
+router.get("/", async (req, res) => {
+    const { lat, lon } = req.query;
+
+    if (!lat || !lon) {
+        return res.status(400).json({
+            error: "You need to provide both lat and lon of location",
+        });
+    }
+
+    const resp = await getCurrentWeatherByLonAndLat({ lat, lon });
+    res.send(resp);
 });
+
+router.get("/zip", async (req, res) => {
+    let { zipcode, countryCode } = req.query;
+
+    if (!zipcode) {
+        return res.status(400).json({
+            error: "You need to provide a zipcode of the location",
+        });
+    }
+
+    if (!countryCode) {
+        countryCode = "US";
+    }
+
+    const { lat, lon } = await getGeocodeLocationFromZip({
+        zipcode,
+        countryCode,
+    });
+    const response = await getCurrentWeatherByLonAndLat({ lat, lon });
+    res.send(response);
+});
+
+async function getCurrentWeatherByLonAndLat({ lat, lon }) {
+    const response = await fetch(
+        `${openWeatherBaseUrl}?` +
+            `lat=${lat}&lon=${lon}&` +
+            `appid=${openWeatherAPIKey}&` +
+            `units=imperial`
+    );
+
+    return await response.json();
+}
+
+async function getGeocodeLocationFromZip({ zipcode, countryCode }) {
+    const response = await fetch(
+        `${geocodeBaseURL}/zip?` +
+            `zip=${zipcode},${countryCode}&` +
+            `appid=${openWeatherAPIKey}`
+    );
+
+    return await response.json();
+}
+
+function formatCurrentWeatherResponseForFrontend(weatherData) {
+    let response = {};
+}
 
 module.exports = router;
