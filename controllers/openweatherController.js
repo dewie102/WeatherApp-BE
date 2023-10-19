5e0a90ff29d3be9338b6605dd584b489dbe002e7
Zack Romasz <zack.romasz@tlgcohort.com>
2023-10-19 16:56:17 -0400

    Updated AirPollution to include all values

--- controllers/openweatherController.js
+++ controllers/openweatherController.js
@@ -1,186 +1,198 @@
 const openWeatherBaseUrl = "https://api.openweathermap.org/data/2.5";
 const geocodeBaseURL = "http://api.openweathermap.org/geo/1.0";
 const airPollutionBaseURL =
-  "http://api.openweathermap.org/data/2.5/air_pollution";
+    "http://api.openweathermap.org/data/2.5/air_pollution";
 const openWeatherAPIKey = process.env.OPENWEATHER_API;
 
 exports.openweather_current_weather_coords = async (req, res) => {
-  const { lat, lon } = req.query;
+    const { lat, lon } = req.query;
 
-  console.log("Hit openweather backend API, lon/lat");
+    console.log("Hit openweather backend API, lon/lat");
 
-  if (!lat || !lon) {
-    return res.status(400).json({
-      error: "You need to provide both lat and lon of location",
-    });
-  }
+    if (!lat || !lon) {
+        return res.status(400).json({
+            error: "You need to provide both lat and lon of location",
+        });
+    }
 
-  const response = await getCurrentWeatherByLonAndLat({ lat, lon });
+    const response = await getCurrentWeatherByLonAndLat({ lat, lon });
 
-  if (Object.hasOwn(response, "cod") && response.cod !== 200) {
-    return res.status(parseInt(response.cod)).json({
-      error: response.message,
-    });
-  }
+    if (Object.hasOwn(response, "cod") && response.cod !== 200) {
+        return res.status(parseInt(response.cod)).json({
+            error: response.message,
+        });
+    }
 
-  const formatted = formatCurrentWeatherResponseForFrontend(response);
-  res.send([formatted]);
+    const formatted = formatCurrentWeatherResponseForFrontend(response);
+    res.send([formatted]);
 };
 
 exports.openweather_lon_lat_by_zip = async (req, res) => {
-  let { zipcode, countryCode } = req.query;
+    let { zipcode, countryCode } = req.query;
 
-  console.log("Hit openweather backend API, zip");
+    console.log("Hit openweather backend API, zip");
 
-  if (!zipcode) {
-    return res.status(400).json({
-      error: "You need to provide a zipcode of the location",
+    if (!zipcode) {
+        return res.status(400).json({
+            error: "You need to provide a zipcode of the location",
+        });
+    }
+
+    if (!countryCode) {
+        countryCode = "US";
+    }
+
+    const response = await getGeocodeLocationFromZip({
+        zipcode,
+        countryCode,
     });
-  }
 
-  if (!countryCode) {
-    countryCode = "US";
-  }
+    if (Object.hasOwn(response, "cod")) {
+        return res.status(parseInt(response.cod)).json({
+            error: response.message,
+        });
+    }
 
-  const response = await getGeocodeLocationFromZip({
-    zipcode,
-    countryCode,
-  });
-
-  if (Object.hasOwn(response, "cod")) {
-    return res.status(parseInt(response.cod)).json({
-      error: response.message,
-    });
-  }
-
-  res.send({ lat: response.lat, lon: response.lon });
+    res.send({ lat: response.lat, lon: response.lon });
 };
 
 exports.openweather_current_airpollution = async (req, res) => {
-  const { lat, lon } = req.query;
+    const { lat, lon } = req.query;
 
-  console.log("Hit openweather air pollution backend API, lon/lat");
+    console.log("Hit openweather air pollution backend API, lon/lat");
 
-  if (!lat || !lon) {
-    return res.status(400).json({
-      error: "You need to provide both lat and lon of location",
-    });
-  }
+    if (!lat || !lon) {
+        return res.status(400).json({
+            error: "You need to provide both lat and lon of location",
+        });
+    }
 
-  const response = await getCurrentAirPollution({ lat, lon });
+    const response = await getCurrentAirPollution({ lat, lon });
 
-  if (Object.hasOwn(response, "cod") && response.cod !== 200) {
-    return res.status(parseInt(response.cod)).json({
-      error: response.message,
-    });
-  }
+    if (Object.hasOwn(response, "cod") && response.cod !== 200) {
+        return res.status(parseInt(response.cod)).json({
+            error: response.message,
+        });
+    }
 
-  const airPollution = getPollutionIndexName(response.list[0].main.aqi);
+    const airPollution = getPollutionIndexName(response.list[0].main.aqi);
 
-  res.send({ airPollution });
+    const result = {
+        so: response.list[0].components.co,
+        no: response.list[0].components.no,
+        no2: response.list[0].components.no2,
+        o3: response.list[0].components.o3,
+        so2: response.list[0].components.so2,
+        pm2_5: response.list[0].components.pm2_5,
+        pm10: response.list[0].components.pm10,
+        nh3: response.list[0].components.nh3,
+        airPollution,
+    };
+
+    res.send(result);
 };
 
 exports.openweather_forecast_weather_coords = async (req, res) => {
-  let { lat, lon, count } = req.query;
+    let { lat, lon, count } = req.query;
 
-  console.log("Hit openweather backend API, Hourly");
+    console.log("Hit openweather backend API, Hourly");
 
-  if (!lat || !lon) {
-    return res.status(400).json({
-      error: "You need to provide both lat and lon of location",
+    if (!lat || !lon) {
+        return res.status(400).json({
+            error: "You need to provide both lat and lon of location",
+        });
+    }
+
+    if (!count) {
+        count = 4;
+    }
+
+    const response = await getForecastWeatherByLonAndLat({ lat, lon, count });
+
+    // For some reason this response has a cod of 200 as a string
+    if (Object.hasOwn(response, "cod") && response.cod !== "200") {
+        return res.status(parseInt(response.cod)).json({
+            error: response.message,
+        });
+    }
+
+    result = [];
+    response.list.forEach((dataPoint) => {
+        const formatted = formatCurrentWeatherResponseForFrontend(dataPoint);
+        result.push(formatted);
     });
-  }
 
-  if (!count) {
-    count = 4;
-  }
-
-  const response = await getForecastWeatherByLonAndLat({ lat, lon, count });
-
-  // For some reason this response has a cod of 200 as a string
-  if (Object.hasOwn(response, "cod") && response.cod !== "200") {
-    return res.status(parseInt(response.cod)).json({
-      error: response.message,
-    });
-  }
-
-  result = [];
-  response.list.forEach((dataPoint) => {
-    const formatted = formatCurrentWeatherResponseForFrontend(dataPoint);
-    result.push(formatted);
-  });
-
-  res.send(result);
+    res.send(result);
 };
 
 async function getCurrentWeatherByLonAndLat({ lat, lon }) {
-  const weatherData = await fetch(
-    `${openWeatherBaseUrl}/weather?` +
-      `lat=${lat}&lon=${lon}&` +
-      `appid=${openWeatherAPIKey}&` +
-      `units=imperial`
-  );
+    const weatherData = await fetch(
+        `${openWeatherBaseUrl}/weather?` +
+            `lat=${lat}&lon=${lon}&` +
+            `appid=${openWeatherAPIKey}&` +
+            `units=imperial`
+    );
 
-  const response = await weatherData.json();
-  return response;
+    const response = await weatherData.json();
+    return response;
 }
 
 async function getCurrentAirPollution({ lat, lon }) {
-  const airPollution = await fetch(
-    `${airPollutionBaseURL}?` +
-      `lat=${lat}&lon=${lon}&` +
-      `appid=${openWeatherAPIKey}`
-  );
+    const airPollution = await fetch(
+        `${airPollutionBaseURL}?` +
+            `lat=${lat}&lon=${lon}&` +
+            `appid=${openWeatherAPIKey}`
+    );
 
-  const response = await airPollution.json();
-  return response;
+    const response = await airPollution.json();
+    return response;
 }
 
 async function getGeocodeLocationFromZip({ zipcode, countryCode }) {
-  const response = await fetch(
-    `${geocodeBaseURL}/zip?` +
-      `zip=${zipcode},${countryCode}&` +
-      `appid=${openWeatherAPIKey}`
-  );
+    const response = await fetch(
+        `${geocodeBaseURL}/zip?` +
+            `zip=${zipcode},${countryCode}&` +
+            `appid=${openWeatherAPIKey}`
+    );
 
-  return await response.json();
+    return await response.json();
 }
 
 async function getForecastWeatherByLonAndLat({ lat, lon, count }) {
-  const weatherData = await fetch(
-    `${openWeatherBaseUrl}/forecast?` +
-      `lat=${lat}&lon=${lon}&` +
-      `appid=${openWeatherAPIKey}&` +
-      `units=imperial&` +
-      `cnt=${count}`
-  );
+    const weatherData = await fetch(
+        `${openWeatherBaseUrl}/forecast?` +
+            `lat=${lat}&lon=${lon}&` +
+            `appid=${openWeatherAPIKey}&` +
+            `units=imperial&` +
+            `cnt=${count}`
+    );
 
-  return await weatherData.json();
+    return await weatherData.json();
 }
 
 function formatCurrentWeatherResponseForFrontend(weatherData) {
-  let response = {
-    temp: weatherData.main.temp,
-    humidity: weatherData.main.humidity,
-    windSpeed: weatherData.wind.speed,
-    description: weatherData.weather[0].main,
-    iconUrl: `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`,
-    datetime: weatherData.dt_txt,
-  };
+    let response = {
+        temp: weatherData.main.temp,
+        humidity: weatherData.main.humidity,
+        windSpeed: weatherData.wind.speed,
+        description: weatherData.weather[0].main,
+        iconUrl: `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`,
+        datetime: weatherData.dt_txt,
+    };
 
-  return response;
+    return response;
 }
 
 function getPollutionIndexName(index) {
-  if (index === 1) {
-    return "Good";
-  } else if (index === 2) {
-    return "Fair";
-  } else if (index === 3) {
-    return "Moderate";
-  } else if (index === 4) {
-    return "Poor";
-  } else if (index === 5) {
-    return "Very Poor";
-  }
+    if (index === 1) {
+        return "Good";
+    } else if (index === 2) {
+        return "Fair";
+    } else if (index === 3) {
+        return "Moderate";
+    } else if (index === 4) {
+        return "Poor";
+    } else if (index === 5) {
+        return "Very Poor";
+    }
 }
